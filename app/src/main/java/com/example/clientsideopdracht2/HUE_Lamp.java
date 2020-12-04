af93ad1cf7b0122c056dfd904199123e93b2069e
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2020-12-04 12:44:55 +0100

    Changed the DetailAdapter

--- app/src/main/java/com/example/clientsideopdracht2/HUE_Lamp.java
+++ app/src/main/java/com/example/clientsideopdracht2/HUE_Lamp.java
@@ -5,35 +5,27 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.List;
-
 /**
  * Makes an instance of the HUE_Lamp
  */
 public class HUE_Lamp {
-    private final static String log = HUE_Lamp.class.getSimpleName();
+    private final static String tag = HUE_Lamp.class.getSimpleName();
 
-    private String ModelId;
     private String name = "lamp";
     private State state;
 
     public HUE_Lamp(JSONObject jsonObject) {
         try {
-            this.ModelId = jsonObject.getString("modelid");
             this.name = jsonObject.getString("name");
             this.state = new State(jsonObject.getJSONObject("state"));
         } catch (JSONException exception) {
-            Log.e(log, "Error with Json");
+            Log.e(tag, "Error with Json");
             exception.printStackTrace();
         }
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getModelId() {
-        return ModelId;
     }
 
     public State getState() {
