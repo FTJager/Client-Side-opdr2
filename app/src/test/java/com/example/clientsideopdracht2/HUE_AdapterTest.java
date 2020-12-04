1c2744771b69be3ced7d46df01e7647b7b8fbb86
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2020-12-04 14:57:29 +0100

    Test

---
+++ app/src/test/java/com/example/clientsideopdracht2/HUE_AdapterTest.java
@@ -0,0 +1,31 @@
+package com.example.clientsideopdracht2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HUE_AdapterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void onCreateViewHolder() {
+
+    }
+
+    @Test
+    void onBindViewHolder() {
+    }
+
+    @Test
+    void getItemCount() {
+    }
+}
