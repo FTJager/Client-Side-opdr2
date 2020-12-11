c767b836438c3a59fbd9a452b1270731dae598f4
Bipin <floriantjager@gmail.com>
2020-12-11 11:37:18 +0100

    DDDD

--- app/src/test/java/com/example/clientsideopdracht2/HUE_LampTest.java
+++ app/src/test/java/com/example/clientsideopdracht2/HUE_LampTest.java
@@ -1,21 +1,38 @@
 package com.example.clientsideopdracht2;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class HUE_LampTest {
+
+    @Mock
     JSONObject lampJSON = Mockito.mock(JSONObject.class);
     JSONObject stateJSON = Mockito.mock(JSONObject.class);
+
+    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @BeforeEach
     void setUp() {
         try {
+            Map<String, Object> stateMap = new HashMap<>();
+
             stateJSON.put("on", false);
             stateJSON.put("bri", 1);
             stateJSON.put("hue", 10000);
@@ -29,6 +46,7 @@ class HUE_LampTest {
             lampJSON.put("state", state);
             lampJSON.put("modelid", "LCT007");
             lampJSON.put("name", "Hue color lamp 1");
+
         }catch (JSONException exception){
             exception.printStackTrace();
         }
@@ -36,7 +54,11 @@ class HUE_LampTest {
 
     @Test
     void getId() {
-        final HUE_Lamp lamp = new HUE_Lamp(lampJSON, 2);
+        //final HUE_Lamp lamp = new HUE_Lamp(lampJSON, 2);
+        HUE_Lamp lamp = Mockito.mock(HUE_Lamp.class);
+
+        when(lamp.getId()).thenCallRealMethod();
+
         assertEquals(2, lamp.getId());
     }
 
